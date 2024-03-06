4eb12081ca3a747acde3fe1ac94ab14c03a2d3ae
Donny Yang <144220166+donny098765@users.noreply.github.com>
2024-03-06 14:11:47 -0600

    Add files via upload

---
+++ portfolio/js/site.js
@@ -0,0 +1,5 @@
+const icon = document.querySelector('.icon')
+
+icon.addEventListener('click', function() {
+    window.location.href = 'C:/Users/donny/OneDrive/Desktop/my_projects/portfolio/A07-Certificate_Starter';
+})
